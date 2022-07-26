c09591ebcffabb81014aebf94b09bcfc53c69d1d
Abdukarimov Ziyodulla <ZiyodullaDeveloper@gmail.com>
2022-07-26 11:57:42 +0000

    config edited

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 API_TOKEN = 'BOT_TOKENI'
 admin_id = 5137963318 #admin id raqami
-=======
-API_TOKEN = 'BOT_TOKEN'
-admin_id = admin_id_number
->>>>>>> c5f883cf335adb8a746020ccb000159fff658832
